071f0b00731648cc188cf2f5059f1b5fb9692d42
Christian Alares <christian.alares@gmail.com>
2020-07-15 14:51:53 +0200

    add code

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  env: {
+    node: true,
+    commonjs: true,
+    es6: true
+  },
+  extends: ['eslint:recommended', 'prettier'],
+  globals: {
+    process: 'readonly'
+  },
+  parserOptions: {
+    ecmaVersion: 2018
+  },
+  plugins: ['prettier'],
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        singleQuote: true,
+        semi: false,
+        useTabs: false,
+        tabWidth: 2,
+        printWidth: 100
+      }
+    ]
+  }
+}
--- index.js
+++ index.js
@@ -1,7 +1,30 @@
 const simpleGit = require('simple-git')
+const inquirer = require('inquirer')
+const { exec } = require('child_process')
 
 const git = simpleGit({
   baseDir: process.cwd(),
 })
 
-console.log(git.branch())
+git.branchLocal(async (_commands, output) => {
+  const answer = await inquirer.prompt([
+    {
+      type: 'list',
+      name: 'branch',
+      message: 'Choose branch:',
+      default: '3',
+      choices: output.all,
+      default: output.current,
+    },
+  ])
+
+  exec(`git checkout ${answer.branch}`, (err, stdout, stderr) => {
+    if (err) {
+      console.log(stderr)
+      process.exit(0)
+    }
+
+    console.log(stdout, stderr)
+    process.exit(0)
+  })
+})
