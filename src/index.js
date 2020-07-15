8ccd1f7c97838f11feaf49919b038e84a46f21d3
Christian Alares <christian.alares@gmail.com>
2020-07-15 15:46:42 +0200

    Bail out if cwd is not a git repo

--- src/index.js
+++ src/index.js
@@ -2,17 +2,24 @@ const simpleGit = require('simple-git')
 const inquirer = require('inquirer')
 const { exec } = require('child_process')
 
+const thisFolder = process.cwd()
+
 const git = simpleGit({
-  baseDir: process.cwd(),
+  baseDir: thisFolder,
 })
 
 git.branchLocal(async (_commands, output) => {
+  if (!output) {
+    console.log(`😕 This folder (${thisFolder}) is not a git repository.`)
+    process.exit(0)
+  }
+
   const answer = await inquirer.prompt([
     {
       type: 'list',
       name: 'branch',
       message: 'Choose branch:',
-      choices: output.all,
+      choices: output.all.map(b => b),
       default: output.current,
     },
   ])
